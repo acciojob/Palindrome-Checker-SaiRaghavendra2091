d98d55aac26fbaaaa470dc63af07d97a69b995b6
SaiRaghavendra2091 <101471458+SaiRaghavendra2091@users.noreply.github.com>
2022-10-30 23:18:44 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+  let i = 0, j = str.length();
+  
+  while(i < j){
+    if(str.charAt(i) == str.charAt(j)){
+      i++;
+      j--;
+    }
+    else
+      break;
+  }
 }
 module.exports = palindrome
